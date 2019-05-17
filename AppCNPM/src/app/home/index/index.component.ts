b144254e6ece5de03034e576b14ce9e5fd8151ac
DESKTOP-IJFJJCG\MINH <huynhtony08@gmail.com>
2019-05-17 16:12:00 +0700

    [Minh] commit check date book room

--- AppCNPM/src/app/home/index/index.component.ts
+++ AppCNPM/src/app/home/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  public addForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.addForm = this.formBuilder.group({
+      DateStart: [formatDate(new Date(), 'yyyy-MM-dd', 'en'), [Validators.required]],
+      DateEnd: [formatDate(new Date(), 'yyyy-MM-dd', 'en'), [Validators.required]],
 
-  constructor() { }
+    });
+  }
 
   ngOnInit() {
   }
 
+  OnBookNow() {
+    if (this.addForm.get('DateStart').value >= this.addForm.get('DateEnd').value
+      || this.addForm.get('DateStart').value < formatDate(new Date(), 'yyyy-MM-dd', 'en')) {
+      alert('Ngày Bắt đầu phải nhỏ hơn ngày kết thúc và lớn hơn ngày hiện tại ');
+    }
+  }
 }
