0adb07c9ba203db9aa6e4e69b0ba842da6830aec
Hiep <anhlanguoingungoc@gmail.com>
2019-05-30 23:14:27 +0700

    [Hiệp] Thêm funciton add phiếu dịch vụ

--- ScriptData/service.sql
+++ ScriptData/service.sql
@@ -38,3 +38,34 @@ BEGIN
    set idDatPhong=@IdDatPhong,NgayDat=@NgayDat,idDichVu=idDichVu,SoLuong=@SoLuong,Gia=@Gia,TinhTrang=@TinhTrang,idNv=@idNv
    where ID =@ID
 END
+
+CREATE PROC [dbo].[NHAP_PHIEU_DICH_VU] (
+ @idDatPhong int,
+ @NgayDat character(20),
+ @idDichVu int,
+ @SoLuong int,
+ @Gia int,
+ @TinhTrang int,
+ @idNv int
+) AS
+BEGIN
+   insert into PhieuDichVu(idDatPhong,NgayDat,idDichVu,SoLuong,Gia,0,idNv)
+    values(@idDatPhong,@NgayDat,@idDichVu,@SoLuong,@Gia,@TinhTrang,@idNv)
+END
+
+CREATE PROC [dbo].[LAY_DANH_SACH_PHIEU_DICH_VU]  
+AS
+BEGIN
+   select ID,idDatPhong,NgayDat,idDichVu,SoLuong,Gia,TinhTrang,idNv
+   from PhieuDichVu
+   where TinhTrang=0
+END
+
+CREATE PROC [dbo].[TIM_DANH_SACH_PHIEU_DICH_VU] 
+  @idDatPhong int 
+AS
+BEGIN
+   select ID,idDatPhong,NgayDat,idDichVu,SoLuong,Gia,TinhTrang,idNv
+   from PhieuDichVu
+   where idDatPhong =@idDatPhong
+END
